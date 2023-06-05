979454d262792b21096cfbb27d0c8e138c8fc7de
ForkEyeee <shawncarter123456@gmail.com>
2023-06-05 16:18:31 -0400

    working on insert method

--- scripts.js
+++ scripts.js
@@ -5,28 +5,24 @@ class Node {
     this.right = right
   }
 }
-let root = null
 class Tree {
   constructor(arr) {
     this.arr = arr
   }
 
+  root = null
+
   buildTree(arr = this.sortArray(this.arr), start = 0, end = arr.length - 1) {
     if (start > end) {
-      // console.log(`Returning null: start (${start}) > end (${end})`);
-      return null;
+      return null
     }
-    const sortedArray = this.sortArray(this.arr);
-    let mid = Math.floor((start + end) / 2);
-    const node = new Node(sortedArray[mid]);
-    // console.log(`Creating node with data: ${node.data}`);
-    // console.log(`Creating left child: start (${start}), end (${mid - 1})`);
-    node.left = this.buildTree(sortedArray, start, mid - 1);
-    // console.log(`Creating right child: start (${mid + 1}), end (${end})`);
-    node.right = this.buildTree(sortedArray, mid + 1, end);
-    // console.log(node);
-    return node;
-}
+    const sortedArray = this.sortArray(this.arr)
+    let mid = Math.floor((start + end) / 2)
+    const node = new Node(sortedArray[mid])
+    node.left = this.buildTree(sortedArray, start, mid - 1)
+    node.right = this.buildTree(sortedArray, mid + 1, end)
+    return node
+  }
 
   sortArray(array) {
     const sortedArray = this.mergeSort(array)
@@ -53,7 +49,7 @@ class Tree {
     return arr.concat(firstHalf.slice().concat(secondHalf.slice()))
   }
 
-  prettyPrint (node = this.buildTree(), prefix = '', isLeft = true) {
+  prettyPrint(node = this.buildTree(), prefix = '', isLeft = true) {
     if (node === null) {
       return
     }
@@ -68,14 +64,28 @@ class Tree {
     if (node.left !== null) {
       this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true)
     }
-		const arr = this.arr
-		const n = arr.length
-		root = this.buildTree(this.arr, 0, n - 1);
+    const arr = this.arr
+    const n = arr.length
+    this.root = this.buildTree(this.arr, 0, n - 1)
   }
-	insert(value) {
-
-	}
+  insert(value) {
+		const newNode = new Node(value)
+    let currentNode = this.root
+    while (currentNode.left !== null || currentNode.right !== null) {
+      if (currentNode.left !== null && value > currentNode.left.data) {
+        currentNode = currentNode.left
+      } else {
+        currentNode = currentNode.right
+      }
+    }
+		if(value < currentNode.data) {
+			currentNode.left = newNode
+		} else {
+			currentNode.right = newNode
+		}
+  }
 }
 
-const test = new Tree([1,2,3,4,5])
+const test = new Tree([2, 4, 6, 8, 10])
 test.prettyPrint()
+test.insert(3)
